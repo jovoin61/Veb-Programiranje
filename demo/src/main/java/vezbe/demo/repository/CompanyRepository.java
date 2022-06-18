b007e2fc95d6c2bb9789aef88bd44608d9306702
jovoin61 <guzvic.in61.2020@uns.ac.rs>
2022-06-18 16:59:24 +0200

    Brisanje foldera koji je višak

--- demo/src/main/java/vezbe/demo/repository/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package vezbe.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import vezbe.demo.model.Company;
-
-@Repository
-public interface CompanyRepository extends JpaRepository<Company, Long> {
-}
--- demo/src/main/java/vezbe/demo/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package vezbe.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import vezbe.demo.model.Department;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
