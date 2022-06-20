e331db64cb5e99cfdf8bc1e6fcaa1d921a7d3868
jovoin61 <guzvic.in61.2020@uns.ac.rs>
2022-06-20 18:06:05 +0200

    Izmena

---
+++ Veb Projekat/veb/src/main/java/com/example/veb/controller/AdminRestController.java
@@ -0,0 +1,7 @@
+package com.example.veb.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminRestController {
+}
---
+++ Veb Projekat/veb/src/main/java/com/example/veb/controller/KorisnikRestController.java
@@ -0,0 +1,4 @@
+package com.example.veb.controller;
+
+public class KorisnikRestController {
+}
